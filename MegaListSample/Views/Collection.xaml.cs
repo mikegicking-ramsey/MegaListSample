f85c7192fffe6d7df58a567d69a01bed239f7a8c
Mike Gicking <mike.gicking@daveramsey.com>
2020-07-24 15:02:58 -0500

    Initial commit

---
+++ MegaListSample/Views/Collection.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace MegaListSample.Views
+{
+    public partial class Collection : ContentPage
+    {
+        public ObservableCollection<string> items = new ObservableCollection<string>();
+
+        public Collection()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                var skip = (i % 26) + 65;
+                items.Add(Convert.ToChar(skip).ToString());
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            MegaCollectionView.ItemsSource = items;
+
+            base.OnAppearing();
+        }
+    }
+}
---
+++ MegaListSample/Views/List.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace MegaListSample.Views
+{
+    public partial class List : ContentPage
+    {
+        public ObservableCollection<string> items = new ObservableCollection<string>();
+
+        public List()
+        {
+            InitializeComponent();
+
+            for(int i = 1; i <= 10000; i++)
+            {
+                var skip = (i % 26) + 65;
+                items.Add(Convert.ToChar(skip).ToString());
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            MegaListView.ItemsSource = items;
+
+            base.OnAppearing();
+        }
+    }
+}
